61cc1a8dbb6f9653125c6dae631e24482e4139eb
Radek <radekie@wp.pl>
2021-11-27 19:03:44 +0100

    zad1 v1.0

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.body;
+let timer = null;
 
+stopButton.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timer = setInterval(() => {
+    let backgroundColor = getRandomHexColor();
+    body.style.backgroundColor = backgroundColor;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  stopButton.disabled = true;
+  startButton.disabled = false;
+  clearInterval(timer);
+})
